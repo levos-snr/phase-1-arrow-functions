2c433ee8c3ceae43f8afa1d70da96f3c0de53060
Lewis Odero <lewisodero27@gmail.com>
2024-07-24 12:20:33 +0300

    test: pass tests for arrow functions

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+
+const divide = (a = 2000, b = 100) =>{
+        return a/b
+        
+}
+
+const square = (x = 2) =>{
+        return x*x
+}
+
+const add = (a = 3, b = 4) => {
+
+        return a+b
+}
+
